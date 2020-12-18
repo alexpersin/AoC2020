536926f855990e8095c9480296ee4375924a0cf9
Alex Persin <alex@Alex-desktop.localdomain>
2020-12-18 18:18:42 +0000

    18

--- 18/p1.py
+++ 18/p1.py
@@ -1,74 +1,85 @@
 with open("inputs/18", "r") as f:
     inputs = f.readlines()
 
-"""
-send characters into the coroutine
-coroutine evaluates expression
 
-loop over characters
-if number, apply operator to accumulator and number
-if operator, set operator
-if open bracket send characters into evaluate until bracket count is 0 
-receive result as number
-"""
+def tokenize(line):
+    for char in line:
+        if char != " ":
+            yield char
 
-class GetVal(Exception):
-    pass
-
-def maths(line):
-    print(f"Evaluating {line}")
-    def evaluate():
-        accumulator = 0
-        open_brackets = 0
-        operator = "+"
-        subgroup_evaluate = evaluate()
-        expr = ''
-        s = False
+def parse(tokenizer):
+    children = []
+    try:
         while True:
-            try:
-                token = yield
-                expr += token
-                if token == ' ':
-                    continue
-                print(token)
+            token = next(tokenizer)
+            if token == '(':
+                children.append(parse(tokenizer))
+            elif token == ')':
+                return children
+            else:
+                children.append(token)
+    except StopIteration:
+        return children
 
-                if token == ')':
-                    open_brackets -= 1
-                if token == '(':
-                    open_brackets += 1
-                if open_brackets > 0:
-                    print(f"Depth {open_brackets} sending {token} to subroutine")
-                    if not s:
-                        next(subgroup_evaluate)
-                        s = True
-                    subgroup_evaluate.send(token)
-                elif token == ')':
-                    open_brackets -= 1
-                    if open_brackets == 0:
-                        subresult = subgroup_evaluate.throw(GetVal)
-                        print(f"Got subresult {subresult}")
-                        print(f"acc: {accumulator} {operator} {subresult}")
-                        accumulator = eval(f"{accumulator} {operator} {subresult}")
-                    print(f"{open_brackets} open brackets")
-                elif token.isnumeric():
-                    print(f"acc: {accumulator} {operator} {token}")
-                    accumulator = eval(f"{accumulator} {operator} {token}")
-                elif token == '(':
-                    open_brackets += 1
-                    print(f"{open_brackets} open brackets")
-                elif token in ("-+*/"):
-                    operator = token
-            except GetVal:
-                print(f"Exiting {expr} subroutine with val {accumulator} and token {token}")
-                yield accumulator
-        
-        return
+def evaluate(expr):
+    acc = 0
+    op = "+"
+    for token in expr:
+        if isinstance(token, list):
+            val = evaluate(token)
+            acc = eval(f"{acc} {op} {val}")
+        elif token in "+*":
+            op = token
+        else:
+            acc = eval(f"{acc} {op} {token}")
+    return acc
 
-    ev = evaluate()
-    next(ev)
-    for token in line:
-        ev.send(token)
-    return int(ev.throw(GetVal))
+def evaluate_2(expr):
+    expr = iter(expr)
+    output = [next(expr)]
+    for item in expr:
+        try:
+            item = int(item)
+        except:
+            pass
 
-print(maths("1 + (2 * 3) + (4 * (5 + 6))"))
-# print(sum(maths(line) for line in inputs))
+        if output[-1] == '+':
+            # Add the current item to the last value
+            if isinstance(item, int):
+                output.pop()
+                last_val = output.pop()
+                output.append(last_val + item)
+            else:
+                output.pop()
+                last_val = output.pop()
+                output.append(last_val + evaluate_2(item))
+        else:
+            output.append(item)
+    
+    print(output)
+
+"""
+For item in list 
+if int, add to output 
+if item is a list, evaluate it and add to list
+if item is a plus, set last item to this plus that
+if item is a mult, ignore
+
+rerun
+"""
+
+
+for line in inputs:
+    print(line.strip())
+    print(parse(tokenize(line.strip())))
+    print(evaluate_2(parse(tokenize(line.strip()))))
+    print()
+
+"""
+if token is int and next token is op, int goes to left of op, op becomes current
+if token is op with no r and next is int, int goes to right
+if token is mult and next token is plus, right becomes right of plus, mult becomes left of plus
+"""
+
+
+print(sum(evaluate(parse(tokenize(line.strip()))) for line in inputs))
