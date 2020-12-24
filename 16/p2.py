f92accf29953a3742f2974f5ef9cf79fee04e36d
Alex Persin <apersin11@gmail.com>
2020-12-24 14:46:47 +0000

    horrible 16

--- 16/p2.py
+++ 16/p2.py
@@ -6,9 +6,9 @@ from collections import defaultdict
 from copy import deepcopy
 
 with open("inputs/16", 'r') as f:
-    data = f.read()
+    data = f.read().strip()
 
-fields, ticket, nearby = data.split("\n\n")
+fields, TICKET, nearby = data.split("\n\n")
 
 def get_valid_tickets():
     pairs = []
@@ -28,24 +28,20 @@ def get_valid_tickets():
             valid[i] = 1
 
     valid_tkts = []
-    for tkt in nearby.split("\n")[1:] + [ticket.split('\n')[-1]]:
+    for tkt in nearby.split("\n")[1:]: # + [ticket.split('\n')[-1]]:
         for field in tkt.split(","):
             f = int(field)
             if f >= len(valid) or not valid[f]:
                 break
         else:
-            valid_tkts.append(sorted([int(f) for f in tkt.split(",")]))
+            valid_tkts.append([int(f) for f in tkt.split(",")])
 
     return valid_tkts
 
 tickets = get_valid_tickets()
-
-print(fields)
-
-# field_values = [[]] * len(valid_tkts[0])
-# for tkt in valid_tkts:
-#     for i, f in enumerate(tkt):
-#         field_values[i].append(f)
+print("Valid tickets:")
+for t in tickets:
+    print(t)
 
 tkt_fields = {}
 names = set()
@@ -53,22 +49,30 @@ for field in fields.split("\n"):
     name, a, _, b = field.rsplit(' ', 3)
     names.add(name[:-1])
     tkt_fields[name[:-1]] = [[int(x) for x in a.split('-')], [int(x) for x in b.split('-')]]
+print("Contstraints:")
+for i in tkt_fields.items():
+    print(i)
 
-print(names)
-possible_fields = [deepcopy(names)] * len(tickets[0])
+values = []
+possible_fields = []
+for i in range(len(tickets[0])):
+    possible_fields.append(deepcopy(names))
+    values.append([])
 
 for j, ticket in enumerate(tickets):
     for i, val in enumerate(ticket):
+        values[i].append(val)
+
+print(values)
+
+for i, vals in enumerate(values):
+    for val in vals:
         # check each constraint for the remaining fields for that row, if it fails remove it
-        to_remove = set()
-        for field in possible_fields[i]:
+        for field in list(possible_fields[i]):
             c1, c2 = tkt_fields[field]
-            if  val < c1[0] or (val > c1[1] and val < c2[0]) or val > c2[1]:
-                print(f"Ticket {j}: value {val} for row {i} is invalid with constaints {c1} {c2}")
-                to_remove.add(field)
-        if to_remove and i == 6:
-            print(f"Removing {to_remove} from {possible_fields[i]}")
-        possible_fields[i] = possible_fields[i] - to_remove
+            if not ((val >= c1[0] and val <= c1[1]) or (val >= c2[0] and val <= c2[1])):
+                print(f"{val} is invalid with {c1} {c2}. Position {i} cannot be {field}")
+                possible_fields[i].remove(field)
 
 csp = defaultdict(set)
 for i, v in enumerate(possible_fields):
@@ -77,7 +81,6 @@ for i, v in enumerate(possible_fields):
         csp[field].add(i)
 
 for k, v in csp.items():
-
     a = []
     for i in range(20):
         if i in v:
@@ -86,25 +89,29 @@ for k, v in csp.items():
             a.append(' ')
     print(f"{k[:16]:16} {''.join(a)}")
 
+answer = {}
+changed = True
+while changed:
+    changed = False
+    for pos, fields in list(enumerate(possible_fields)):
+        if pos in answer:
+            continue
+        if len(fields) == 1:
+            changed = True
+            field = list(fields)[0]
+            print(f"pos {pos} is {field}")
+            answer[pos] = field
+            for a, b in list(enumerate(possible_fields)):
+                if a != pos:
+                    b.discard(field)
 
-
-# print(tkt_fields)
-# possible_fields = []
-
-# for j, l in enumerate(field_values):
-#     p = []
-#     for field in tkt_fields:
-#         for mn, mx in field[1:]:
-#             print(mn, mx)
-#             if not all(x>=mn and x<=mx for x in l):
-#                 print(f"Field {field[0]} not valid for row {j}")
-#                 break
-#         else:
-#             p.append(tkt_fields[0])
-#     possible_fields.append(p)
-
-# print(possible_fields)
-# """
-# get range for each row
-# get possible fields for each row
-# """
+TICKET = [int(t.strip()) for t in TICKET.replace('your ticket:\n', '').strip().split(',')]
+res = 1
+for k, v in answer.items():
+    if v.startswith('departure'):
+        val = TICKET[k]
+        print(k, v, val)
+        res *= val
+print(res)
+print(TICKET)
+# 6320160
